e132db0ae27431547bae82794b4030931ebf00e5
rockts <rockts@sina.com>
2025-12-21 02:06:41 +0800

    style: darken footer background to distinguish from CTA section

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,7 +4,7 @@ import { Facebook, Github, Linkedin, Rss } from 'lucide-react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-900 text-white pt-10 pb-6 border-t border-gray-800">
+    <footer className="bg-black text-white pt-10 pb-6 border-t border-gray-900">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-8">
           {/* Company Info */}
